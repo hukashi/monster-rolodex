731fee82e22a83314cb5f8ab65d24363d187ce5b
Jonathan Chung <jonathan.chung@sooryen.com>
2020-02-26 21:29:13 -0500

    updated with cards and start of search box

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
 
 class App extends Component {
-  
   constructor() {
     super();
     this.state = {
-      string: "This is Jonathan"
+      monsters: [],
+      searchField: ""
     };
   }
-  
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {this.state.string}
-          </p>
-          <button onClick={ () => this.setState({ string: 'Hello Mason'}) }>Click This</button>
-        </header>
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
